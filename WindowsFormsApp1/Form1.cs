9664a7b54e36d285ee4041ca32d6360308fb8a39
prodject <anonymous.prodject@gmail.com>
2021-02-16 19:48:50 +0300

    изменён тип переменных

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,9 +19,9 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int a = Convert.ToInt32(textBox1.Text);
-            int b = Convert.ToInt32(textBox2.Text);
-            int c;
+            real a = Convert.ToInt32(textBox1.Text);
+            real b = Convert.ToInt32(textBox2.Text);
+            real c;
             c = a + b;
             textBox3.Text = Convert.ToString(c);
 
